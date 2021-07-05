a5514602dc80c8791fe5df519c38647a5e95c03d
alysonmiguel <alysonmiguel16@gmail.com>
2021-07-05 20:49:39 -0300

    first commit

---
+++ src/UDPServer.java
@@ -0,0 +1,23 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+public class UDPServer {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket udpsocket = new DatagramSocket(6789);
+        byte[] buffer = new byte[1000];
+        while (true){
+            DatagramPacket recebe = new DatagramPacket(buffer, buffer.length);
+            udpsocket.receive(recebe);
+            byte[] bufferrecebido = recebe.getData();
+            InetAddress ip = recebe.getAddress();
+            int port = recebe.getPort();
+            System.out.println("Recebeu: " + new String (bufferrecebido, StandardCharsets.UTF_8));
+            String saida = new String (bufferrecebido, StandardCharsets.UTF_8);
+            saida = saida.toUpperCase();
+            DatagramPacket responde = new DatagramPacket (saida.getBytes(), saida.getBytes().length, ip, port);
+            udpsocket.send(responde);
+        }
+    }
+}
