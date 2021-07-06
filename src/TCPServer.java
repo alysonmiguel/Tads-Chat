5236c785f9aeab473ab53d4b5515049876a286a4
alysonmiguel <alysonmiguel16@gmail.com>
2021-07-06 19:50:51 -0300

    TCP

---
+++ src/TCPServer.java
@@ -0,0 +1,21 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class TCPServer {
+    public static void main(String argv[]) throws Exception{
+        String palavra;
+        String novaPalavra;
+        ServerSocket socket = new ServerSocket(6789);
+        while(true){
+            Socket conexao = socket.accept();
+            DataInputStream recebe = new DataInputStream (conexao.getInputStream());
+            DataOutputStream responde = new DataOutputStream(conexao.getOutputStream());
+            palavra = recebe.readUTF();
+            System.out.println("Recebeu: " + palavra);
+            novaPalavra = palavra.toUpperCase();
+            responde.writeUTF(novaPalavra);
+        }
+    }
+}
